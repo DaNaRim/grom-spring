26421a1e8c8a00678dd8cf75d51d07c184e084ab
DaNaRim <rangar.danarim@gmail.com>
2020-09-08 19:16:10 +0300

    homework2

--- src/main/java/com/homework2/controller/ItemController.java
+++ src/main/java/com/homework2/controller/ItemController.java
@@ -5,88 +5,85 @@ import com.homework2.exception.InternalServerException;
 import com.homework2.model.Item;
 import com.homework2.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@WebServlet(urlPatterns = "/test")
-public class ItemController extends HttpServlet {
+@Controller
+@RequestMapping(value = "/item")
+public class ItemController {
 
     @Autowired
     private ItemService itemService;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
+    @RequestMapping(method = RequestMethod.GET, params = {"id"}, produces = "text/plain")
+    public @ResponseBody
+    String doGet(@RequestParam(value = "id") String id) {
         try {
-            long id = Long.parseLong(req.getParameter("id"));
-            Item item = itemService.findById(id);
+            Item item = itemService.findById(Long.parseLong(id));
 
-            resp.getWriter().println(item.toString());
-            resp.getWriter().println("Get success");
+            return item.toString() + "\r\n" + "Get success";
 
         } catch (BadRequestException | InternalServerException e) {
 
-            resp.getWriter().println("Get failed");
             System.err.println(e.getMessage());
+            return "Get failed";
         }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
+    @RequestMapping(method = RequestMethod.POST, params = {"name", "description"},
+            produces = "text/plain")
+    public @ResponseBody
+    String doPost(@RequestParam(value = "name") String name,
+                  @RequestParam(value = "description") String description) {
         try {
-            Item item = new Item(
-                    req.getParameter("name"),
-                    req.getParameter("description")
-            );
+            Item item = new Item(name, description);
 
             itemService.save(item);
-            resp.getWriter().println("Post success");
+            return "Post success";
 
         } catch (BadRequestException | InternalServerException e) {
 
-            resp.getWriter().println("Post failed");
             System.err.println(e.getMessage());
+            return "Post failed";
         }
     }
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
+    @RequestMapping(method = RequestMethod.PUT, params = {"id", "name", "description"},
+            produces = "text/plain")
+    public @ResponseBody
+    String doPut(@RequestParam(value = "id") String id,
+                 @RequestParam(value = "name") String name,
+                 @RequestParam(value = "description") String description) {
         try {
-            Item item = new Item(
-                    req.getParameter("name"),
-                    req.getParameter("description")
-            );
-            long id = Long.parseLong(req.getParameter("id"));
-            item.setId(id);
+            Item item = new Item(name, description);
+
+            item.setId(Long.parseLong(id));
 
             itemService.update(item);
-            resp.getWriter().println("Put success");
+            return "Put success";
 
         } catch (BadRequestException | InternalServerException e) {
-
-            resp.getWriter().println("Put failed");
             System.err.println(e.getMessage());
+            return "Put failed";
+
         }
     }
 
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @RequestMapping(method = RequestMethod.DELETE, params = {"id"}, produces = "text/plain")
+    public @ResponseBody
+    String doDelete(@RequestParam(value = "id") String id) {
         try {
-            long id = Long.parseLong(req.getParameter("id"));
-            itemService.delete(id);
+            itemService.delete(Long.parseLong(id));
 
-            resp.getWriter().println("Delete success");
+            return "Delete success";
 
         } catch (InternalServerException | BadRequestException e) {
-
-            resp.getWriter().println("Delete failed");
             System.err.println(e.getMessage());
+            return "Delete failed";
         }
     }
 
